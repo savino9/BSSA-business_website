3cefb952c341ca7304a83ef443936b009d2e3f6e
Savino Rasola <savinorasola90@gmail.com>
2018-08-25 20:09:03 +0200

    update product page + adding swiper npm package + update style product page

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
-  $('.carousel').carousel()
-
+	// alert("js working");
+  //  video background
 	var vid = document.getElementById('video');
 
 	$('#myBtn').click(function() {
@@ -13,6 +13,52 @@ $( document ).ready(function() {
 			$('#myBtn').html("Pause");
 		}
   });
+
+  // carousel start
+  $('.carousel').carousel();
+
+  //-- Single product page 
+	//-- Click on detail
+	$("ul.menu-items > li").on("click",function(){
+	  $("ul.menu-items > li").removeClass("active");
+	  $(this).addClass("active");
+	})
+
+	$(".attr,.attr2").on("click",function(){
+	  var clase = $(this).attr("class");
+	  $("." + clase).removeClass("active");
+	  $(this).addClass("active");
+	})
+
+	//-- Click on QUANTITY
+	$(".btn-minus").on("click",function(){
+	  var now = $(".section > div > input").val();
+	  if ($.isNumeric(now)){
+	    if (parseInt(now) -1 > 0){ now--;}
+	    $(".section > div > input").val(now);
+	  }else{
+	    $(".section > div > input").val("1");
+	  }
+	})            
+	$(".btn-plus").on("click",function(){
+	  var now = $(".section > div > input").val();
+	  if ($.isNumeric(now)){
+	    $(".section > div > input").val(parseInt(now)+1);
+	  }else{
+	    $(".section > div > input").val("1");
+	  }
+	})       
+
+	// --------------------------------------
+	// modal
+	$('#exampleModalCenter').css({"z-index": "-1"});	
+	$("#buy-btn").on("click", function() {
+		// alert("ok");
+		$('#exampleModalCenter').modal('show');	
+		$('#exampleModalCenter').css({"z-index": "2000"});
+	}) 
+	
+
 
 });
 
