e1e85b351d5c0e8560418b25e46a17996c764e3a
Savino Rasola <savinorasola90@gmail.com>
2018-08-28 16:02:00 +0200

    Node assignment complete!

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
 
+// var options = {
+//   dotfiles: 'ignore',
+//   etag: false,
+//   extensions: ['htm', 'html', 'css', 'js','mp4'],
+//   index: false,
+//   maxAge: '1d',
+//   redirect: false,
+//   setHeaders: function (res, path, stat) {
+//     res.set('x-timestamp', Date.now())
+//   }
+// }
+
 app.use(express.static('public'))
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
